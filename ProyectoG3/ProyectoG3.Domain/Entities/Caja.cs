c510955e556e337a4253e9afbf377618ea0343c8
Gaelgudel <Gaelgudel@gmail.com>
2026-03-05 20:58:52 -0600

    Se Dejó el modulo de Cajas funcional al 90%

--- ProyectoG3/ProyectoG3.Domain/Entities/Caja.cs
+++ ProyectoG3/ProyectoG3.Domain/Entities/Caja.cs
@@ -1,20 +1,30 @@
 ﻿using ProyectoG3.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProyectoG3.Models
 {
+    [Table("CAJAS")]
     public class Caja
     {
         [Key]
         public int IdCaja { get; set; }
+
+        [ForeignKey("Comercio")]
         public int IdComercio { get; set; }
-        public required string Nombre { get; set; }
-        public required string TelefonoSINPE { get; set; }
-        public required string Descripcion { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string TelefonoSINPE { get; set; }
+
+        public string Descripcion { get; set; }
+
         public DateTime FechaDeRegistro { get; set; }
+
         public DateTime? FechaDeModificacion { get; set; }
+
         public bool Estado { get; set; }
+
         public virtual Comercio? Comercio { get; set; }
-       // public virtual ICollection<Sinpe> Sinpes { get; set; } = new List<Sinpe>();
     }
 }
