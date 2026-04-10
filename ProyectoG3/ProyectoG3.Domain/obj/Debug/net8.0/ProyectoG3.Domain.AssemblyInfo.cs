1b631f9b51bbf5c3ed822f3087b4463dda342a92
Firomolina <amolina60792@gmail.com>
2026-04-10 09:12:20 -0600

    rollback

--- ProyectoG3/ProyectoG3.Domain/obj/Debug/net8.0/ProyectoG3.Domain.AssemblyInfo.cs
+++ ProyectoG3/ProyectoG3.Domain/obj/Debug/net8.0/ProyectoG3.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoG3.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44297e0c164f4198415b68ad7f90ac9cb770d592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be6b6a0593f0a3e05f8eb3ea321649acb6cce0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoG3.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoG3.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
