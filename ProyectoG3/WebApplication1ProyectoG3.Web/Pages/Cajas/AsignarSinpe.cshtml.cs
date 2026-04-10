972c36b549e7414e17594b185b051b8f95d9aa4b
Firomolina <amolina60792@gmail.com>
2026-04-10 08:51:22 -0600

    Reapply "Ajuste de rutas"
    This reverts commit 83882206de7fc3eca6526e68620a8ac17837f8e3.

--- ProyectoG3/WebApplication1ProyectoG3.Web/Pages/Cajas/AsignarSinpe.cshtml.cs
+++ ProyectoG3/WebApplication1ProyectoG3.Web/Pages/Cajas/AsignarSinpe.cshtml.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ProyectoG3.Infrastructure.Persistence;
 using ProyectoG3.Domain.Entities;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace ProyectoG3.Web.Pages.Cajas
 {
@@ -23,11 +21,40 @@ namespace ProyectoG3.Web.Pages.Cajas
         [BindProperty]
         public int IdCaja { get; set; }
 
-        public List<SelectListItem> Cajas { get; set; }
+        [BindProperty]
+        public int IdCajaOrigen { get; set; }
+
+        // IdComercio para saber a dónde volver si no hay caja asignada
+        [BindProperty]
+        public int IdComercioOrigen { get; set; }
+
+        public List<SelectListItem> Cajas { get; set; } = new();
 
         public void OnGet(int idSinpe)
         {
             IdSinpe = idSinpe;
+
+            var sinpe = _context.Sinpes.FirstOrDefault(s => s.IdSinpe == idSinpe);
+
+            if (sinpe != null && sinpe.IdCaja.HasValue)
+            {
+                // Si ya tiene caja asignada, guardamos su id y el comercio al que pertenece
+                IdCajaOrigen = sinpe.IdCaja.Value;
+                var caja = _context.Cajas.FirstOrDefault(c => c.IdCaja == sinpe.IdCaja.Value);
+                if (caja != null)
+                    IdComercioOrigen = caja.IdComercio;
+            }
+            else if (sinpe != null)
+            {
+                // Si no tiene caja, buscamos por teléfono destinatario
+                var caja = _context.Cajas
+                    .FirstOrDefault(c => c.TelefonoSINPE == sinpe.TelefonoDestinatario);
+                if (caja != null)
+                {
+                    IdCajaOrigen = caja.IdCaja;
+                    IdComercioOrigen = caja.IdComercio;
+                }
+            }
 
             Cajas = _context.Cajas
                 .Select(c => new SelectListItem
@@ -42,16 +69,17 @@ namespace ProyectoG3.Web.Pages.Cajas
             var sinpe = _context.Sinpes.FirstOrDefault(s => s.IdSinpe == IdSinpe);
 
             if (sinpe == null)
-            {
                 return NotFound();
-            }
 
-            //Ligar Sinpe
             sinpe.IdCaja = IdCaja;
-
             _context.SaveChanges();
 
-            return RedirectToPage("/Index");
+            // Si tenemos el id de la caja origen volvemos a VerSinpe
+            // Si no, volvemos a Comercios
+            if (IdCajaOrigen > 0)
+                return RedirectToPage("/Cajas/VerSinpe", new { id = IdCajaOrigen });
+
+            return RedirectToPage("/Comercios/Index");
         }
     }
 }
